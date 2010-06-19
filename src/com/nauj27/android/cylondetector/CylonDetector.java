c9b6a82b04b9aae99077b838d87ed149ac039935
nauj27 <nauj27@arriba.nauj27.com>
2010-06-19 21:32:53 +0200

    Initial import

---
+++ src/com/nauj27/android/cylondetector/CylonDetector.java
@@ -0,0 +1,13 @@
+package com.nauj27.android.cylondetector;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CylonDetector extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
