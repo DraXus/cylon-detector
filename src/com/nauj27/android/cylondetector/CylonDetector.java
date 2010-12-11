12396b73795ecf7b5acad73fb5af5d9085ee151b
Juan Mart?n <android@nauj27.com>
2010-12-11 23:01:18 +0100

    Show preview from camera and optional image on the right. Get a valid preview size (the minimum) from de API.

--- src/com/nauj27/android/cylondetector/CylonDetector.java
+++ src/com/nauj27/android/cylondetector/CylonDetector.java
@@ -1,13 +1,18 @@
 package com.nauj27.android.cylondetector;
 
 import java.io.IOException;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CylonDetector extends Activity {
 	
@@ -20,7 +25,19 @@ public class CylonDetector extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        // From http://www.designerandroid.com/?p=73
+    	// This is the only way camera preview work on all android devices
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		
+		// No title, no name: Full screen
+		getWindow().setFormat(PixelFormat.TRANSLUCENT);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+		// Set the main layout for the application
+		setContentView(R.layout.main);
         
         // Get the surface view where to put the camera images
 		SurfaceView surfaceView = (SurfaceView)findViewById(R.id.SurfaceViewCamera);
@@ -34,22 +51,18 @@ public class CylonDetector extends Activity {
     }
     
 	/**
-	 * Create the surface callback for the surface holder of the camera.
+	 * Create the surface call back for the surface holder of the camera.
 	 */
 	private SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
 		
 		@Override
-		public void surfaceCreated(SurfaceHolder surfaceHolder) {		
+		public void surfaceCreated(SurfaceHolder surfaceHolder) {	
+			
+			// Obtain an instance of Camera.
+			// See http://developer.android.com/reference/android/hardware/Camera.html
+			// for more information of the whole  process.
 			camera = Camera.open();
 			
-			try {
-				camera.setPreviewDisplay(surfaceHolder);
-			} catch (IOException ioException) {
-				if (camera != null) {
-					camera.release();
-					camera = null;
-				}
-			}
 		}
 
 		@Override
@@ -59,16 +72,38 @@ public class CylonDetector extends Activity {
 				int width, 
 				int height) {
 			
-			// http://developer.android.com/reference/android/hardware/Camera.html
-			Camera.Parameters cameraParameters = camera.getParameters();
-			
-			// TODO: GET THE VALID PREVIEW SIZE WIDTH AND HEIGHT FOR THE CAMERA
-			
-			cameraParameters.setPreviewSize(PREVIEW_SIZE_WIDTH, PREVIEW_SIZE_HEIGHT);
-			cameraParameters.setPictureFormat(PixelFormat.JPEG);
-			
-			camera.setParameters(cameraParameters);
-			camera.startPreview();
+			if (camera != null) {
+				Camera.Parameters cameraParameters = camera.getParameters();
+				
+				// Get the supported preview sizes
+				List<Camera.Size> supportedPreviewSizes = cameraParameters.getSupportedPreviewSizes();
+				
+				// Search for the minimum camera preview size, just check width
+				int previewSizeIndex = 0;
+				for (int index = 0; index < supportedPreviewSizes.size(); index++) {
+					if (supportedPreviewSizes.get(index).width < supportedPreviewSizes.get(previewSizeIndex).width) {
+						previewSizeIndex = index;
+					}
+				}
+				Camera.Size cameraSize = supportedPreviewSizes.get(previewSizeIndex);
+				cameraParameters.setPreviewSize(cameraSize.width, cameraSize.height);
+				
+				// Set auto white balance
+				cameraParameters.setWhiteBalance(Camera.Parameters.WHITE_BALANCE_AUTO);
+				
+				camera.setParameters(cameraParameters);
+				
+				try {
+					camera.setPreviewDisplay(surfaceHolder);
+				} catch (IOException e) {
+					// TODO: SHOW TO THE USER THAT CAN'T SHOW CAMERA IMAGE
+					// AND RETURN
+				}
+				
+				camera.startPreview();
+			} else {
+				// TODO: SHOW TO THE USER THAT CAN'T OPEN THE CAMERA
+			}
 		}
 		
 		@Override
